b67526dc5751bd06c23b9bd56ed9f10b0ade2f41
micholewa26 <michallejwoda@biskupiak.pl>
2025-12-15 16:24:05 +0100

    [zejsciowka]

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,37 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* obiekt;
+
+public:
+    ResourceManager() : obiekt(new Resource()) {}
+    ~ResourceManager() { delete obiekt; }
+    ResourceManager(const ResourceManager& kopia) { obiekt = new Resource(*kopia.obiekt); }
+    ResourceManager& operator=(const ResourceManager& kopia)
+    {
+        if (this == &kopia) {
+            return *this;
+        };
+        delete obiekt;
+        obiekt = new Resource(*kopia.obiekt);
+        return *this;
+    }
+
+    double get() { return obiekt->get(); }
+
+    ResourceManager(ResourceManager&& arg) : obiekt(arg.obiekt) { arg.obiekt = nullptr; }
+
+    ResourceManager& operator=(ResourceManager&& arg)
+    {
+        if (this == &arg) {
+            return *this;
+        }
+
+        delete obiekt;
+        obiekt     = arg.obiekt;
+        arg.obiekt = nullptr;
+
+        return *this;
+    }
 };
